5356dd991fd5b35d507479a17a2651a31abe30f4
AJ Sigmon <alexandersigmon@gmail.com>
2016-08-04 15:25:12 -0400

    origin

---
+++ BugTracker/Models/BugTracker/zHistory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BugTracker.Models.BugTracker
+{
+    public class zHistory
+    {
+        public int id { get; set; }
+        public DateTimeOffset Created { get; set; }
+        public DateTimeOffset Updated { get; set; }
+        public string Assignee { get; set; }
+        public string State { get; set; }
+
+    }
+}
